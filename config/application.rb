90f151205d5bde2fdf3414fe261dd3db5b062686
Elton Galeti <egaleti2003@gmail.com>
2024-05-22 15:37:28 -0300

    'Commit generateds devise models'

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module LoginScreen
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.eager_load_paths << Rails.root.join("extras")``
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
 end
